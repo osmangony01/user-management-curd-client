d80536520acfb8423451c8ee4783d9b579f474e2
osmangony01 <mdosmangoni376@gmail.com>
2023-05-10 15:11:59 +0600

    delete a user from the database successfully

--- src/components/Home/User.jsx
+++ src/components/Home/User.jsx
@@ -8,7 +8,35 @@ const User = ({ user, users, setUsers }) => {
     const { _id, name, email, gender, status } = user;
 
     const handleDelete = _id => {
-       
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/user/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                        const remaining = users.filter(user => user._id !== _id);
+                        setUsers(remaining);
+
+                    })
+            }
+        })
 
     }
     return (
